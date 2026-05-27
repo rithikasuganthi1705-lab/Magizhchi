a16e7738036f27e5ff31b9b52f7eb7da7d872557
unknown <rithikasuganthi1705@gmail.com>
2026-05-27 20:22:03 +0530

    doing assignment

---
+++ Task.java
@@ -0,0 +1,11 @@
+class Task
+{
+	public static void main(String[]args)
+	{
+		System.out.println("*****Super Market*****");
+		System.out.println("1	Fruits	100");
+		System.out.println("2	Milk	30");
+		System.out.println("3	Ice	70");
+		System.out.println("* * * * * * * * * * *");
+	}
+}
