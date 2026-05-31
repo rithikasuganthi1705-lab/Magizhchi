c54a510d1d99af92e3b37c1e1ed242dfd2138727
unknown <rithikasuganthi1705@gmail.com>
2026-05-31 17:13:57 +0530

    revision

---
+++ Stores.java
@@ -0,0 +1,43 @@
+class Stores
+{
+	public static void main(String[]args)
+	{
+		String name = "ABC STORES";
+		String para = "Item Qty Price Amount";
+		String rice = "Rice";
+		short quantity = 2;
+		float price = 60.0f;
+		float amount = 120.0f;
+		String sugar = "Sugar";
+		short quan = 1;
+		float pri = 45.0f;
+		float  amt = 45.0f;
+		double grand = 165.0;
+		String res = " THANKYOU! VISIT AGIN";
+		System.out.println("============================");
+		System.out.println(name);
+		
+		System.out.println("============================");
+		System.out.println(para);
+		System.out.println("----------------------------");
+		System.out.println(rice + "\t" + quantity + "\t"+ price + "\t" + amount);
+		System.out.println(sugar + "\t" + quan + "\t" + pri + "\t" + amt);
+		System.out.println("Grant Total:" + grand);
+		System.out.println("=============================");
+		System.out.println(res);
+		System.out.println("=============================");
+	}
+}
+		
+		
+		
+		
+		
+		
+        
+   		
+		
+		
+		
+		
+		
