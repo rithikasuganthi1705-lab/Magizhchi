a73163cc5e22dfd100b2c27c68526163c0e8c757
unknown <rithikasuganthi1705@gmail.com>
2026-06-03 06:30:23 +0530

    operators

---
+++ Calc.java
@@ -0,0 +1,17 @@
+class Calc
+{
+	public static void main(String[]args)
+	{
+		int a = 100;
+		int b = 50;
+		System.out.println("A = " + a);
+		System.out.println("B = " + b);
+		System.out.println("ADDITION : " + (a+b));
+		System.out.println("SUBTRACTION : " + (a-b));
+		System.out.println("MULTIPLICATION : " + (a*b));
+		System.out.println("DIVISION : " + (a/b));
+	}
+}
+
+		
+		
---
+++ RelOp.java
@@ -0,0 +1,22 @@
+class RelOp
+{
+	public static void main(String[]args)
+	{
+		int a = 10;
+		int b = 20;
+		System.out.println("A = " + a);
+		System.out.println("B = " + b);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+	}
+}
+		
+		
+		
+		
+		
+		
