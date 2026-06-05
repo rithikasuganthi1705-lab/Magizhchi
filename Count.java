dd9fc012376e153fa143d16bc66037df8499651a
unknown <rithikasuganthi1705@gmail.com>
2026-06-05 22:22:41 +0530

    loop

---
+++ Count.java
@@ -0,0 +1,21 @@
+class Count
+{
+	public static void main(String[]args)
+	{
+		int number = 987;
+			System.out.println(number);
+		
+		int count = 0;
+		while(number>0)
+		{
+			number = number/10;
+			count++;
+		}
+				
+			System.out.println(count);
+		
+		
+		
+	}
+}
+		
