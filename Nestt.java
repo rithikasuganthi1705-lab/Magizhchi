a07028f32d0f5d3589287616356df2dd33ca6749
unknown <rithikasuganthi1705@gmail.com>
2026-06-08 22:40:05 +0530

    nestedloop

---
+++ Nestt.java
@@ -0,0 +1,20 @@
+class Nestt
+{
+	public static void main(String[]args)
+	{
+		char alph = 'A';
+		for(int i=1 ; i<=5 ; i++)
+		{
+			for(int j=1 ; j<=i; j++)
+			{
+				System.out.print(alph);
+				alph++;
+			}
+			System.out.println();
+		}
+			
+	
+	}
+}	
+			
+			
